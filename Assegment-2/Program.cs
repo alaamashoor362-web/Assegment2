6a75cd36476f30f667779aec573995605079542a
Alaa Mashhour <alaamashoor362@gmail.com>
2026-08-20 15:41:12 +0300

    Add project files.

---
+++ Assegment-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assegment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
